26fd5d857c690ed934cf530df6e00c684316423c
user1896 <abde.user15@gmail.com>
2023-11-27 03:35:55 +0100

    define the method 'next()' outside of the @@iterator

--- iteratorObj.js
+++ iteratorObj.js
@@ -12,25 +12,46 @@ function makeDragon(){
     }
 }
 
-const dragonArmy = {
-    [Symbol.iterator]: () => {
-        return{
-            next: () => {
-                enoughDragonSpawned = Math.random() > 0.75
-                if( !enoughDragonSpawned ){
-                    return{
-                        value: makeDragon(),
-                        done: false
-                    }
-                }
-                else{
-                    return{done: true}
-                }
+// const dragonArmy = {
+//     [Symbol.iterator]: () => {
+//         return{
+//             next: () => {
+//                 enoughDragonSpawned = Math.random() > 0.75
+//                 if( !enoughDragonSpawned ){
+//                     return{
+//                         value: makeDragon(),
+//                         done: false
+//                     }
+//                 }
+//                 else{
+//                     return{done: true}
+//                 }
+//             }
+//         }
+//     }
+// }
+
+// 
+const dragonArmy2 = {
+    [Symbol.iterator]: function() { // the method must not be an arrow function because it has the "this" keyword
+        return this
+        // the [Symbol.iterator] method should return the iterator, and because we implemented the "next()" method
+        // direcly in the object, we made him an iterator, so we return him ("this" keyword)
+    },
+    next: () => { // by implementing the "next()" method outside the @@iterator we can call it directly if we want
+        enoughDragonSpawned = Math.random() > 0.75
+        if( !enoughDragonSpawned ){
+            return{
+                value: makeDragon(),
+                done: false
             }
+        }
+        else{
+            return{done: true}
         }
     }
 }
 
-for(const dragon of dragonArmy){
+for(const dragon of dragonArmy2){
     console.log(dragon)
 }
