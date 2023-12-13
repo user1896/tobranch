764fef9d6e4b66caf373c3c11d0d6d56b1ae2539
user1896 <abde.user15@gmail.com>
2023-12-13 18:27:11 +0100

    proving that promises are not a syntactic sugar over callback functions

--- promises.js
+++ promises.js
@@ -64,3 +64,28 @@ let mycatch = usingPromises()
     "catch" method to determine what happens if an error occurred (rejected, and the data sent with the function "reject" will be put
     as an argument to the callback function of the method "catch", or it can be put as a second parameter of the callback fct of then()).
 */
+
+// Promises are not just a syntactic sugar
+
+let myPromise = new Promise((resolve) => {
+    console.log(1)
+    resolve('task message')
+    console.log(2)
+})
+
+myPromise.then( msg => {
+    console.log(msg)
+} )
+console.log(3)
+// executed console.log(msg) as a response from the promise that has no asynchronous function, then we did console.log(2), result: 1-2-3-msg
+
+/*
+    We can see that Promises are not just a syntactic sugar that looks nice instead of using cllbacks, because that response that is used in
+    the "resolve" or "reject" will be put into the "Web API" then the "Callback Queue" and it won't be executed until the "call stack" is empty.
+    While if we use only callback functions that we define, they won't leave the "call stack" because they're just normal functions, so they get
+    executed in the order they are called one on top of the other.
+
+    We also notice that the only thing asynchronous about the promise is the response (resolve or reject), and this is way Promises are great
+    to handle asynchronous tasks, because in the Promise's constructor when an async task gets added to the callback queue, we have the response
+    get added as well, so it will be behind it in the queue.
+*/
